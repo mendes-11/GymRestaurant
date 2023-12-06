5f69303600cbd33504339d326d413877ca9db6ce
Renato Mendes <rnt.mendes11@gmail.com>
2023-12-06 16:55:55 -0300

    Add ScreenTotem

--- Front/src/app/app.component.spec.ts
+++ Front/src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'Front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Front');
   });
 
   it('should render title', () => {
--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ApiProductService } from './Services/api-product.service';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Front';
+  menuItems: any;
+  promocao: any;
+  imageUrl = "../assets/tapioca.jpeg";
+
+  constructor(private service: ApiProductService) { }
+
+  ngOnInit(): void {
+    this.service.getAll().subscribe({
+      next: (res) => { 
+        console.log('Dados recebidos:', res); 
+        this.menuItems = res;
+        this.promocao = res.filter(item => item.valorPromocional > 0 && item.valorPromocional < item.valor);
+        console.log('Dados recebidos:', this.menuItems); 
+        console.log('Dados recebidos:', this.promocao); 
+        },
+      error: (err) => {
+        console.error('Erro ao buscar itens:', err);
+      }
+    });
+  }
 }
